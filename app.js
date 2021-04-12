04eae071b0f2d8bdced39ec83dd73d9f07a56c6e
dalong <dalong0514@126.com>
2021-04-12 22:00:04 +0800

    yellow hat - app.js

--- app.js
+++ app.js
@@ -8,14 +8,18 @@ function getUsersHomeFolder() {
   return osenv.home()
 }
 
+// Simple wrapper around the fs.readdir function for getting list of files
 function getFilesInFolder(folderPath, cb) {
   fs.readdir(folderPath, cb)
 }
 
+// Function that combines user’s personal folder path with getting its list of files
 function main() {
   const folderPath = getUsersHomeFolder()
   getFilesInFolder(folderPath, (err, files) => {
+    // Simple message to display in case of error loading folder’s files
     if (err) return alert('Sorry, we could not load your home folder')
+    // For each file in list, logs full path for file to console
     files.forEach(file => {
       console.log(`${folderPath}/${file}`)
     })
