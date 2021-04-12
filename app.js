e1d5bc1e2362908563e3689a5295ded7a2e27cfd
dalong <dalong0514@126.com>
2021-04-13 00:08:19 +0800

    Listing 2.12 Rendering the template instances in the UI via the app.js file

--- app.js
+++ app.js
@@ -39,10 +39,23 @@ function inspectAndDescribeFiles(folderPath, files, cb) {
   }, cb)
 }
 
+// Adds new function called displayFile that handles rendering template instance
+function displayFile(file) {
+  const mainArea = document.getElementById('main-area')
+  const template = document.querySelector('#item-template')
+  // Creates copy of template instance
+  let clone = document.importNode(template.content, true)
+  // Alters instance to include file’s name and icon
+  clone.querySelector('img').src = `images/${file.type}.svg`
+  clone.querySelector('.filename').innerText = file.file
+  // Appends template instance to "mainarea" div element
+  mainArea.appendChild(clone)
+}
+
 // Creates displayFiles function to be end point where files will end up being displayed
 function displayFiles(err, files) {
   if (err) return alert('Sorry, we could not display your files')
-  files.forEach(file => console.log(file))
+  files.forEach(displayFile)
 }
 
 // Function that combines user’s personal folder path with getting its list of files
