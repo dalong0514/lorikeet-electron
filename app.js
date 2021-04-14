e9dc1c3459eda03844b4428ff1b4567a2baf5200
dalong <dalong0514@126.com>
2021-04-15 00:53:37 +0800

    3.1.2 Handling double-clicks on folders

--- app.js
+++ app.js
@@ -7,12 +7,9 @@ const userInterface = require('./userInterface')
 // Function that combines user’s personal folder path with getting its list of files
 function main() {
   userInterface.bindDocument(window)
-  const folderPath = fileSystem.getUsersHomeFolder()
-  fileSystem.getFilesInFolder(folderPath, (err, files) => {
-    // Simple message to display in case of error loading folder’s files
-    if (err) return alert('Sorry, we could not load your home folder')
-    fileSystem.inspectAndDescribeFiles(folderPath, files, userInterface.displayFiles)
-  })
+  let folderPath = fileSystem.getUsersHomeFolder()
+  userInterface.loadDirectory(folderPath)(window)
 }
 
-main()
+// Calls main function after HTML for app has loaded in window
+window.onload = main
--- userInterface.js
+++ userInterface.js
@@ -1,6 +1,35 @@
 'use strict'
 
 let document
+const fileSystem = require('./fileSystem')
+
+// Adds function to display current folder
+function displayFolderPath(folderPath) {
+  document.getElementById('current-folder').innerText = folderPath
+}
+
+// Clears items out of main-area div element
+function clearView() {
+  const mainArea = document.getElementById('main-area')
+  let firstChild = mainArea.firstChild
+  while (firstChild) {
+    mainArea.removeChild(firstChild)
+    firstChild = mainArea.firstChild
+  }
+}
+
+// loadDirectory changes current folder path and updates main area
+function loadDirectory(folderPath) {
+  return function (window) {
+    if (!document) document = window.document
+    displayFolderPath(folderPath)
+    fileSystem.getFilesInFolder(folderPath, (err, files) => {
+      clearView()
+      if (err) return alert('Sorry, you could not load your folder')
+      fileSystem.inspectAndDescribeFiles(folderPath, files, displayFiles)
+    })
+  }
+}
 
 // Adds new function called displayFile that handles rendering template instance
 function displayFile(file) {
@@ -10,6 +39,13 @@ function displayFile(file) {
   let clone = document.importNode(template.content, true)
   // Alters instance to include file’s name and icon
   clone.querySelector('img').src = `images/${file.type}.svg`
+  // Adds double-click event listener to icon if it’s for a directory
+  if (file.type === 'directory') {
+    clone.querySelector('img')
+      .addEventListener('dblclick', () => {
+        loadDirectory(file.path)()
+      }, false)
+  }
   clone.querySelector('.filename').innerText = file.file
   // Appends template instance to "mainarea" div element
   mainArea.appendChild(clone)
@@ -25,7 +61,9 @@ function bindDocument (window) {
   if (!document) document = window.document
 }
 
+// Makes sure loadDirectory function is exposed as public API
 module.exports = {
   bindDocument,
-  displayFiles
+  displayFiles,
+  loadDirectory
 }
